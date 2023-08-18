9d87fb3d153ffcb356ca500c7a0cccb1f88b8386
Samuel <samuel@iMac-de-Andres.local>
2023-08-18 11:46:24 -0500

    test shell task 2

--- shell.c
+++ shell.c
@@ -6,43 +6,50 @@ int main()
     size_t arg_line_len = 0;
     char *line = NULL;
     char *token;
-    const char delimiter[] = " ";
+    const char delimiter[] = " \n\t";
     pid_t pid;
     char *str[5];
-    int i = 0;
-	int status; 
-
+    int i = 1;
+    int status;
+	
     while(1)
     {
-        write(1, "($)", 4);
-        line = malloc(1024 * sizeof(char)); 
-        if (line == NULL)
-            exit(1);
+		line = NULL;
+        i = 1;
+        write(1, "($) ", 5);
         line_len = getline(&line, &arg_line_len, stdin);
         if (line_len == EOF)
         {
             free(line);
             exit(0);
         }
-        token = strtok(line, delimiter); 
+        token = strtok(line, delimiter);
+        str[0] = token;
         while (token != NULL)
         {
-            str[i] = token;
             token = strtok(NULL, delimiter);
+            str[i] = token;
             i++;
         }
         str[i] = NULL;
         pid = fork();
         if (pid == -1)
-            exit (1);
+		{
+			free(line);
+			exit (0);
+		}
         else if (pid == 0)
         {
-            execve(str[0], str, NULL); 
-			perror("execve");
-			exit(1);
-        } 
-		else
-			wait(&status);
+            free(line);
+            if(execve(str[0], str, NULL) == -1)
+            {
+                perror("execve");
+                exit(0);
+            }
+        }
+        else
+            wait(&status);
+        free(line);
     }
     return (0);
 }
