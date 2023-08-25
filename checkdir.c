680caaaf4b2beb7a6153909ffe0367087d400f39
Samuel <samuel@iMac-de-Andres.local>
2023-08-25 00:03:05 -0500

    upload updates

--- checkdir.c
+++ checkdir.c
@@ -1,104 +1,106 @@
 #include "main.h"
 
+/**
+* checkdir - check directories to find executable file
+* @str: tokenized input line
+*
+* Return: 0
+*/
+
 extern char **environ;
 
-/**
- * checkdir - check directories to find executable file
- * @str: tokenized input line
- *
- * Return: 0
- */
 int checkdir(char **str)
 {
-	char *var[128], *command, temp[128];
-	int i = 1;
-	const char delimiter[] = ":"; /*salto linea no tenia sentido*/
-	struct stat file_stat;
-	char *env = malloc(1024); /**/
-
-	if (env == NULL)
-		return (0);
-	strcpy(env, getenv("PATH"));
-	var[0] = strtok(env, delimiter);
-	while (1)
-	{
-		var[i] = strtok(NULL, delimiter);
-		if (var[i] == NULL)
-			break;
-		i++;
-	}
-	if (stat(str[0], &file_stat) == 0)
-	{
-		forkshell(str[0], str, env);
-		return (0);
-	}
-	command = malloc(32); 
-	if (command == NULL)
-		return (0);
-
-	strcpy(command, "/");
-	strcat(command, str[0]);
-	for (i = 0; var[i] != NULL; i++)
-	{
-		strcpy(temp, var[i]);
-		strcat(temp, command);
-		if (stat(temp, &file_stat) == 0) /*tener 2 stats no tiene sentido*/
-		{
-			free(command);
-			forkshell(temp, str, env);
-			return (0);
-		}
-	}
-	fprintf(stderr, "./hsh: 1: %s: not found\n", str[0]);
-	free(env);
-	free(command);
-	return (0);
+    char *var[128], *command, temp[128];
+    int i = 1;
+    const char delimiter[] = ":";
+    struct stat file_stat;
+    char *env = malloc(1024);
+    if (env == NULL)
+        return (0);
+    strcpy(env, getenv("PATH"));
+    if (getenv("PATH") == NULL)
+        return (0);
+    var[0] = strtok(env, delimiter);
+    while (1)
+    {
+        var[i] = strtok(NULL, delimiter);
+        if (var[i] == NULL)
+            break;
+        i++;
+    }
+    if (stat(str[0], &file_stat) == 0)
+    {
+        forkshell(str[0], str, env);
+        return (0);
+    }
+    command = malloc(32);
+    if (command == NULL)
+        return (0);
+    strcpy(command, "/");
+    strcat(command, str[0]);
+    for (i = 0; var[i] != NULL; i++)
+    {
+        strcpy(temp, var[i]);
+        strcat(temp, command);
+        if (stat(temp, &file_stat) == 0)
+        {
+            free(command);
+            forkshell(temp, str, env);
+            return (0);
+        }
+    }
+    fprintf(stderr, "./hsh: 1: %s: not found\n", str[0]);
+    free(env);
+    free(command);
+    return (0);
 }
-
 /**
- * forkshell - when command is found, it creates the fork
- * @fileloc: location of the executable file
- * @env: memory to free
- * @str: arguments
- *
- * Return: 0
- */
+* forkshell - when command is found, it creates the fork
+* @file: location of the executable file
+* @env: memory to free
+* @str: arguments
+*
+* Return: 0
+*/
 int forkshell(char *file, char **str, char *env)
 {
-	pid_t pid;
-	int status;
-	char **envp = environ;
-
-	pid = fork();
-	if (pid == -1)
-	{
-		exit(1);
-	}
-	else if (pid == 0)
-	{
-		if (execve(file, str, envp) == -1)
-		{
-			perror("execve");
-			exit(2);
-		}
-	}
-	else
-	{
-		wait(&status);
-		free(env);
-	}
-	return (0);
+    pid_t pid;
+    int status;
+    char **envp = environ;
+    pid = fork();
+    if (pid == -1)
+    {
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        if (execve(file, str, envp) == -1)
+        {
+            perror("execve");
+            exit(2);
+        }
+    }
+    else
+    {
+        wait(&status);
+        free(env);
+    }
+    return (0);
 }
-
+/**
+* printenv - prints environment
+* @line: bring file to free and save space in main
+* Return: 0 on success
+*/
 int printenv(char *line)
 {
     char **env = environ;
-
-    while (*env != NULL) {
+    while (*env != NULL)
+    {
         printf("%s\n", *env);
-        env++; 
+        env++;
     }
-
-	free(line);
+    free(line);
     return (0);
 }
--- shell.c
+++ shell.c
@@ -1,53 +1,55 @@
 #include "main.h"
-
 /**
- * main - simple shell
- *
- * Return: 0 when exiting
- */
-/*falta impresion de errores, 
-status de salida : leer WEXITSTATUS
-poner error cuando le pasemos echo en modo no interactivo. si corre echo "ëcho hola" | ./hsh 
-
+* main - simple shell
+*
+* Return: 0 when exiting
 */
+
 int main(void)
 {
-	ssize_t line_len = 0;
-	size_t arg_line_len = 0;
-	char *line = NULL, *token, *str[5];
-	const char delimiter[] = " \n\t\0";
-	int i = 1;
-
-	while (1)
-	{
-		line = NULL;
-		i = 1;
-		if (isatty(STDIN_FILENO))
-			write(1, "($) ", 5);
-		line_len = getline(&line, &arg_line_len, stdin);
-		if (line_len == EOF || (strcmp(line, "exit\n") == 0))
-		{
-			free(line);
-			exit(0);
-		}
-		token = strtok(line, delimiter);
-		str[0] = token;
-		if (strcmp(str[0], "env") == 0)
-			printenv(line);
-		else if (token != NULL)
-		{
-			while (token != NULL)
-			{
-				token = strtok(NULL, delimiter);
-				str[i] = token;
-				i++;
-			}
-			str[i] = NULL;
-			checkdir(str);
-			free(line); 
-		}		
-		else
-			free(line);
-	}
-	return (0);
+    ssize_t line_len = 0;
+    size_t arg_line_len = 0;
+    char *line = NULL, *token, *str[5];
+    const char delimiter[] = " \n\t\0";
+    int i = 1;
+    while (1)
+    {
+        line = NULL;
+        i = 1;
+        if (isatty(STDIN_FILENO))
+            write(1, "($) ", 5);
+        line_len = getline(&line, &arg_line_len, stdin);
+        if (line_len == EOF || (strcmp(line, "exit\n") == 0))
+        {
+            free(line);
+            exit(0);
+        }
+        token = strtok(line, delimiter);
+        str[0] = token;
+        if (str[0] == NULL)
+        {
+            free(line);
+            return(0);
+        }
+        if (strcmp(str[0], "env") == 0)
+            printenv(line);
+        else if (token != NULL)
+        {
+            while (token != NULL)
+            {
+                token = strtok(NULL, delimiter);
+                if (token != NULL)
+                {
+                    str[i] = token;
+                    i++;
+                }
+            }
+            str[i] = NULL;
+            checkdir(str);
+            free(line);
+        }
+        else
+            free(line);
+    }
+    return (0);
 }
