61deed4f3dccb15e46f58b985f95bc65f4cd6988
Samuel <samuel@iMac-de-Andres.local>
2023-08-25 00:09:14 -0500

    upload updates betty

--- checkdir.c
+++ checkdir.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+extern char **environ;
+
 /**
 * checkdir - check directories to find executable file
 * @str: tokenized input line
@@ -7,53 +9,51 @@
 * Return: 0
 */
 
-extern char **environ;
-
 int checkdir(char **str)
 {
-    char *var[128], *command, temp[128];
-    int i = 1;
-    const char delimiter[] = ":";
-    struct stat file_stat;
-    char *env = malloc(1024);
-    if (env == NULL)
-        return (0);
-    strcpy(env, getenv("PATH"));
-    if (getenv("PATH") == NULL)
-        return (0);
-    var[0] = strtok(env, delimiter);
-    while (1)
-    {
-        var[i] = strtok(NULL, delimiter);
-        if (var[i] == NULL)
-            break;
-        i++;
-    }
-    if (stat(str[0], &file_stat) == 0)
-    {
-        forkshell(str[0], str, env);
-        return (0);
-    }
-    command = malloc(32);
-    if (command == NULL)
-        return (0);
-    strcpy(command, "/");
-    strcat(command, str[0]);
-    for (i = 0; var[i] != NULL; i++)
-    {
-        strcpy(temp, var[i]);
-        strcat(temp, command);
-        if (stat(temp, &file_stat) == 0)
-        {
-            free(command);
-            forkshell(temp, str, env);
-            return (0);
-        }
-    }
-    fprintf(stderr, "./hsh: 1: %s: not found\n", str[0]);
-    free(env);
-    free(command);
-    return (0);
+	char *var[128], *command, temp[128];
+	int i = 1;
+	const char delimiter[] = ":";
+	struct stat file_stat;
+	char *env = malloc(1024);
+	if (env == NULL)
+		return (0);
+	strcpy(env, getenv("PATH"));
+	if (getenv("PATH") == NULL)
+		return (0);
+	var[0] = strtok(env, delimiter);
+	while (1)
+	{
+		var[i] = strtok(NULL, delimiter);
+		if (var[i] == NULL)
+			break;
+		i++;
+	}
+	if (stat(str[0], &file_stat) == 0)
+	{
+		forkshell(str[0], str, env);
+		return (0);
+	}
+	command = malloc(32);
+	if (command == NULL)
+		return (0);
+	strcpy(command, "/");
+	strcat(command, str[0]);
+	for (i = 0; var[i] != NULL; i++)
+	{
+		strcpy(temp, var[i]);
+		strcat(temp, command);
+		if (stat(temp, &file_stat) == 0)
+		{
+			free(command);
+			forkshell(temp, str, env);
+			return (0);
+		}
+	}
+	fprintf(stderr, "./hsh: 1: %s: not found\n", str[0]);
+	free(env);
+	free(command);
+	return (0);
 }
 /**
 * forkshell - when command is found, it creates the fork
@@ -65,28 +65,28 @@ int checkdir(char **str)
 */
 int forkshell(char *file, char **str, char *env)
 {
-    pid_t pid;
-    int status;
-    char **envp = environ;
-    pid = fork();
-    if (pid == -1)
-    {
-        exit(1);
-    }
-    else if (pid == 0)
-    {
-        if (execve(file, str, envp) == -1)
-        {
-            perror("execve");
-            exit(2);
-        }
-    }
-    else
-    {
-        wait(&status);
-        free(env);
-    }
-    return (0);
+	pid_t pid;
+	int status;
+	char **envp = environ;
+	pid = fork();
+	if (pid == -1)
+	{
+		exit(1);
+	}
+	else if (pid == 0)
+	{
+		if (execve(file, str, envp) == -1)
+		{
+			perror("execve");
+			exit(2);
+		}
+	}
+	else
+	{
+		wait(&status);
+		free(env);
+	}
+	return (0);
 }
 /**
 * printenv - prints environment
@@ -95,12 +95,12 @@ int forkshell(char *file, char **str, char *env)
 */
 int printenv(char *line)
 {
-    char **env = environ;
-    while (*env != NULL)
-    {
-        printf("%s\n", *env);
-        env++;
-    }
-    free(line);
-    return (0);
+	char **env = environ;
+	while (*env != NULL)
+	{
+		printf("%s\n", *env);
+		env++;
+	}
+	free(line);
+	return (0);
 }
--- shell.c
+++ shell.c
@@ -7,49 +7,49 @@
 
 int main(void)
 {
-    ssize_t line_len = 0;
-    size_t arg_line_len = 0;
-    char *line = NULL, *token, *str[5];
-    const char delimiter[] = " \n\t\0";
-    int i = 1;
-    while (1)
-    {
-        line = NULL;
-        i = 1;
-        if (isatty(STDIN_FILENO))
-            write(1, "($) ", 5);
-        line_len = getline(&line, &arg_line_len, stdin);
-        if (line_len == EOF || (strcmp(line, "exit\n") == 0))
-        {
-            free(line);
-            exit(0);
-        }
-        token = strtok(line, delimiter);
-        str[0] = token;
-        if (str[0] == NULL)
-        {
-            free(line);
-            return(0);
-        }
-        if (strcmp(str[0], "env") == 0)
-            printenv(line);
-        else if (token != NULL)
-        {
-            while (token != NULL)
-            {
-                token = strtok(NULL, delimiter);
-                if (token != NULL)
-                {
-                    str[i] = token;
-                    i++;
-                }
-            }
-            str[i] = NULL;
-            checkdir(str);
-            free(line);
-        }
-        else
-            free(line);
-    }
-    return (0);
+	ssize_t line_len = 0;
+	size_t arg_line_len = 0;
+	char *line = NULL, *token, *str[5];
+	const char delimiter[] = " \n\t\0";
+	int i = 1;
+	while (1)
+	{
+		line = NULL;
+		i = 1;
+		if (isatty(STDIN_FILENO))
+			write(1, "($) ", 5);
+		line_len = getline(&line, &arg_line_len, stdin);
+		if (line_len == EOF || (strcmp(line, "exit\n") == 0))
+		{
+			free(line);
+			exit(0);
+		}
+		token = strtok(line, delimiter);
+		str[0] = token;
+		if (str[0] == NULL)
+		{
+			free(line);
+			return(0);
+		}
+		if (strcmp(str[0], "env") == 0)
+			printenv(line);
+		else if (token != NULL)
+		{
+			while (token != NULL)
+			{
+				token = strtok(NULL, delimiter);
+				if (token != NULL)
+				{
+					str[i] = token;
+					i++;
+				}
+			}
+			str[i] = NULL;
+			checkdir(str);
+			free(line);
+		}
+		else
+			free(line);
+	}
+	return (0);
 }
